2de702fe93529923d0aae97f0293f536f88f7e44
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 19:24:59 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,25 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
-
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def happy_birthday(kids)
+  kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!\n"
+  end
 end
 
+def age_appropriate_birthday(kids)
+  kids.each do |key, value|
+    if value > 12
+      puts "You are too old for this."
+    else
+      puts "Happy Birthday #{key}! You are now #{value} years old!"
+    end
+  end
+end
 
+kids = {
+  'Timmy' => 9,
+  'Sarah' => 6,
+  'Amanda' => 27
+}
+
+happy_birthday(kids)
+
+age_appropriate_birthday(kids)
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,5 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers[:suite_a]
 end
