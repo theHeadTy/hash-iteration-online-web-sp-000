571e7db209049706d2db428dc7f98a2173725207
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 19:24:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@ end
 def age_appropriate_birthday(kids)
   kids.each do |key, value|
     if value > 12
-      puts "You are too old for this"
+      puts "You are too old for this."
     else
       puts "Happy Birthday #{key}! You are now #{value} years old!"
     end
