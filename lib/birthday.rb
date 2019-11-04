8aaef0538b067e6fc907fcad072fee34723533fd
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 19:17:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
-
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def happy_birthday(kids)
+  kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!\n"
+  end
 end
 
+happy_birthday({
+    'Timmy' => 9,
+    'Sarah' => 6,
+    'Amanda' => 27
+})
 
